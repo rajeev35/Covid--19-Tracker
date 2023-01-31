810ae5b73a4fa939e3dd5551d537fe8af6f3daae
Rajeev Kumar <rajeev2020it049@abesit.edu.in>
2023-01-31 11:49:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Covid from "./components/covid.js";
+
+const App = () => {
+  return (
+    
+     <>
+      <Covid/>
+     </> 
+    
+  )
+}
+
+export default App;
